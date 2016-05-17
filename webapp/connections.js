02ddae6225e829a226518dcca273ce2ddde2f55b
Anas Nakawa <anas.nakawa@gmail.com>
2016-05-17 08:22:00 +0300

    adding connections.js file

---
+++ webapp/connections.js
@@ -0,0 +1,9 @@
+sap.ui.define([], function () {
+			return [
+	{
+		"name": "Announcements",
+		"url": "/destinations/shortpoint-sharepoint_announcements/_api/web/lists/GetByTitle('Announcements')/Items?$select=Id,Title,Body",
+		"id": "rest_dest_0"
+	}
+];
+		});
