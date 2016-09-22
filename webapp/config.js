c4046d743c2eed3986458f6f6f490659e62cc2c2
anasnakawa <anas.nakawa@gmail.com>
2016-09-22 15:32:16 +0000

    increase version + remove global variables

--- webapp/config.js
+++ webapp/config.js
@@ -1,53 +1,55 @@
-var version = '3.4.3';
-var sNamespace = "shortpoint-widget-beta";
-
-var versionBuster = version.replace(/\./g, '00');
-var prefix = jQuery.sap.getModulePath( sNamespace );
-if (prefix === ".") {
-    prefix = "";
-}
-
-var webapp = '';
-if(!sap.ushell || !sap.ushell.Container){
-    // required only in Web IDE testing
-    webapp = 'webapp';
-}
-
-var root = prefix + '/~' + versionBuster + '~/' + webapp;
-jQuery.sap.registerModulePath('shortpoint-root', root);
-jQuery.sap.registerModulePath('shortpoint-libs', root + '/libs');
-
-sap.ui.define(['./connections'], function (connections) {
-	"use strict";
+(function() {
+	var version = "3.4.4";
+	var sNamespace = "shortpoint-widget-beta";
 	
-	// map connections
-	var oConnectionTypes = connections.map(function (item) {
+	var versionBuster = version.replace(/\./g, "00");
+	var prefix = jQuery.sap.getModulePath( sNamespace );
+	if (prefix === ".") {
+	    prefix = "";
+	}
+	
+	var webapp = "";
+	if(!sap.ushell || !sap.ushell.Container){
+	    // required only in Web IDE testing
+	    webapp = "webapp";
+	}
+	
+	var root = prefix + "/~" + versionBuster + "~/" + webapp;
+	jQuery.sap.registerModulePath("shortpoint-root", root);
+	jQuery.sap.registerModulePath("shortpoint-libs", root + "/libs");
+	
+	sap.ui.define(["./connections"], function (connections) {
+		"use strict";
+		
+		// map connections
+		var oConnectionTypes = connections.map(function (item) {
+			return {
+				name: item.name,
+				adapter: "rest",
+				data: { url: prefix + item.url },
+				id: item.id
+			};
+		});
+	
+		/**
+		 * ShortPoint configuration
+		 */
+		var oShortpointPreConfig = { 
+			autoInit: false,
+			scrollWrapperSelector: ".shortpoint-scroll-container",
+			platform: "sap",
+			connectionTypes: oConnectionTypes
+		};
+	
+		window.shortpoint_pre_config = oShortpointPreConfig; // eslint-disable-line camelcase
+	
+		/**
+		 * expose all configurations
+		 */
 		return {
-			name: item.name,
-			adapter: "rest",
-			data: { url: prefix + item.url },
-			id: item.id
+			namespace: sNamespace,
+			shortpointPreConfig: oShortpointPreConfig,
+			appVersion: version
 		};
 	});
-
-	/**
-	 * ShortPoint configuration
-	 */
-	var oShortpointPreConfig = { 
-		autoInit: false,
-		scrollWrapperSelector: ".shortpoint-scroll-container",
-		platform: "sap",
-		connectionTypes: oConnectionTypes
-	};
-
-	window.shortpoint_pre_config = oShortpointPreConfig; // eslint-disable-line camelcase
-
-	/**
-	 * expose all configurations
-	 */
-	return {
-		namespace: sNamespace,
-		shortpointPreConfig: oShortpointPreConfig,
-		appVersion: version
-	};
-});
+})();
