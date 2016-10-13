c4c110556e47141a70206a7b4153e3a1c0713f2b
Anas Nakawa <anas.nakawa@gmail.com>
2016-10-13 12:56:00 +0300

    increase version

--- webapp/config.js
+++ webapp/config.js
@@ -1,27 +1,51 @@
-var version = '3.3.0';
-var sNamespace = "shortpoint-widget-beta";
+(function() {
+	var version = "3.4.6";
+	var sNamespace = "shortpoint-widget-beta";
 
-var versionBuster = version.replace(/\./g, '00');
-var prefix = jQuery.sap.getModulePath( sNamespace );
-if (prefix === ".") {
-    prefix = "";
-}
+	var versionBuster = version.replace(/\./g, "00");
+	var prefix = jQuery.sap.getModulePath( sNamespace );
+	if (prefix === ".") {
+	    prefix = "";
+	}
 
-var webapp = '';
-if(!sap.ushell || !sap.ushell.Container){
-    // required only in Web IDE testing
-    webapp = 'webapp';
-}
+	var webapp = "";
+	if(!sap.ushell || !sap.ushell.Container){
+	    // required only in Web IDE testing
+	    webapp = "webapp";
+	}
 
-var root = prefix + '/~' + versionBuster + '~/' + webapp;
-jQuery.sap.registerModulePath('shortpoint-root', root);
-jQuery.sap.registerModulePath('shortpoint-libs', root + '/libs');
+	var root = prefix + "/~" + versionBuster + "~/" + webapp;
+	jQuery.sap.registerModulePath("shortpoint-root", root);
+	jQuery.sap.registerModulePath("shortpoint-libs", root + "/libs");
 
-sap.ui.define(['./connections'], function (connections) {
-	"use strict";
-	
-	// map connections
-	var oConnectionTypes = connections.map(function (item) {
+	sap.ui.define(["./connections"], function (connections) {
+		"use strict";
+
+		// map connections
+		var oConnectionTypes = connections.map(function (item) {
+			return {
+				name: item.name,
+				adapter: "rest",
+				data: { url: prefix + item.url },
+				id: item.id
+			};
+		});
+
+		/**
+		 * ShortPoint configuration
+		 */
+		var oShortpointPreConfig = {
+			autoInit: false,
+			scrollWrapperSelector: ".shortpoint-scroll-container",
+			platform: "sap",
+			connectionTypes: oConnectionTypes
+		};
+
+		window.shortpoint_pre_config = oShortpointPreConfig; // eslint-disable-line camelcase
+
+		/**
+		 * expose all configurations
+		 */
 		return {
 			name: item.name,
 			adapter: "rest",
@@ -29,25 +53,4 @@ sap.ui.define(['./connections'], function (connections) {
 			id: item.id
 		};
 	});
-
-	/**
-	 * ShortPoint configuration
-	 */
-	var oShortpointPreConfig = { 
-		autoInit: false,
-		scrollWrapperSelector: ".shortpoint-scroll-container",
-		platform: "sap",
-		connectionTypes: oConnectionTypes
-	};
-
-	window.shortpoint_pre_config = oShortpointPreConfig; // eslint-disable-line camelcase
-
-	/**
-	 * expose all configurations
-	 */
-	return {
-		namespace: sNamespace,
-		shortpointPreConfig: oShortpointPreConfig,
-		appVersion: version
-	};
-});
+})();
