fcb6775bf47ae0675d4e557d5fcd07ed94dbeb98
Anas Nakawa <anas.nakawa@gmail.com>
2016-10-13 12:56:00 +0300

    version increase

--- webapp/config.js
+++ webapp/config.js
@@ -1,26 +1,26 @@
 (function() {
-	var version = "3.4.5";
+	var version = "3.4.6";
 	var sNamespace = "shortpoint-widget-beta";
-	
+
 	var versionBuster = version.replace(/\./g, "00");
 	var prefix = jQuery.sap.getModulePath( sNamespace );
 	if (prefix === ".") {
 	    prefix = "";
 	}
-	
+
 	var webapp = "";
 	if(!sap.ushell || !sap.ushell.Container){
 	    // required only in Web IDE testing
 	    webapp = "webapp";
 	}
-	
+
 	var root = prefix + "/~" + versionBuster + "~/" + webapp;
 	jQuery.sap.registerModulePath("shortpoint-root", root);
 	jQuery.sap.registerModulePath("shortpoint-libs", root + "/libs");
-	
+
 	sap.ui.define(["./connections"], function (connections) {
 		"use strict";
-		
+
 		// map connections
 		var oConnectionTypes = connections.map(function (item) {
 			return {
@@ -30,19 +30,19 @@
 				id: item.id
 			};
 		});
-	
+
 		/**
 		 * ShortPoint configuration
 		 */
-		var oShortpointPreConfig = { 
+		var oShortpointPreConfig = {
 			autoInit: false,
 			scrollWrapperSelector: ".shortpoint-scroll-container",
 			platform: "sap",
 			connectionTypes: oConnectionTypes
 		};
-	
+
 		window.shortpoint_pre_config = oShortpointPreConfig; // eslint-disable-line camelcase
-	
+
 		/**
 		 * expose all configurations
 		 */
