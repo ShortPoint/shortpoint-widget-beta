2637a8b2463e20d637f64af5bc7aba63458164a9
Anas Nakawa <anas.nakawa@gmail.com>
2017-02-13 21:51:09 +0200

    remove bult in connections

--- webapp/connections.js
+++ webapp/connections.js
@@ -1,9 +1,3 @@
 sap.ui.define([], function () {
-			return [
-	{
-		"name": "Announcements",
-		"url": "/destinations/shortpoint-sharepoint_announcements/_api/web/lists/GetByTitle('Announcements')/Items?$select=Id,Title,Body",
-		"id": "rest_dest_0"
-	}
-];
-		});
+	return [];
+});
