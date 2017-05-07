eb0ec89b9574ced64d8f0db6d1a71bbc96a3c601
anasnakawa <anas.nakawa@gmail.com>
2017-05-07 17:10:09 +0000

    update connections

--- webapp/connections.js
+++ webapp/connections.js
@@ -1,19 +1,9 @@
 sap.ui.define([], function () {
             return [
-    /*{
-        "name": "SharePoint Announcements",
-        "url": "/destinations/shortpoint-sharepoint2013server/_api/web/lists/GetByTitle('Announcements')/Items?$select=Id,Title,Body",
-        "id": "rest_dest_0"
-    },
-    {
-        "name": "ShortPoint News",
-        "url": "/destinations/shortpoint-sharepoint2013server/_api/web/lists/GetByTitle('ShortPoint News')/Items?$select=Id,Title",
-        "id": "rest_dest_1"
-    },*/
-    {
-        "name": "ShortPoint Demos Website - Announcements",
-        "url": "/destinations/shortpoint-demos_shortpoint_com/_api/web/lists/GetByTitle('Announcements')/Items?$select=Id,Title,Body",
-        "id": "rest_dest_2"
-    }
-];
+			    {
+			        "name": "ShortPoint Demos Website - Announcements",
+			        "url": "/destinations/shortpoint-demos_shortpoint_com/_api/web/lists/GetByTitle('Announcements')/Items?$select=Id,Title,Body",
+			        "id": "rest_dest_2"
+			    }
+			];
         });
