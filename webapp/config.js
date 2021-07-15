f4ba54cffb5c6726a251baf17c977172e66d31d1
albertnahas <albert@shortpoint.com>
2021-07-15 17:36:14 +0400

    increase version to 4.7.9

--- webapp/config.js
+++ webapp/config.js
@@ -1,5 +1,5 @@
 (function() {
-	var version = "4.7.8";
+	var version = "4.7.9";
 	var sNamespace = "shortpoint-widget-beta";
 	
 	var versionBuster = version.replace(/\./g, "00");
@@ -50,7 +50,7 @@
 	
 	var root = prefix + "/~" + versionBuster + "~/" + webapp;
 	jQuery.sap.registerModulePath("shortpoint-root", root);
-	jQuery.sap.registerModulePath("shortpoint-libs", "https://d2zzvnmppezkx.cloudfront.net/sap/4.7.8" + "/libs");
+	jQuery.sap.registerModulePath("shortpoint-libs", "https://d2zzvnmppezkx.cloudfront.net/sap/4.7.9" + "/libs");
 	
 	sap.ui.define(["./connections"], function (connections) {
 		"use strict";
