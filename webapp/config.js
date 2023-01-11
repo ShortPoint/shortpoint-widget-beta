448aee202436315a4261c63cedb7fe7cb84b8431
Emmanuel Segun-Lean <eslean20@gmail.com>
2023-01-11 17:32:00 +0100

    increase version to 4.8.0

--- webapp/config.js
+++ webapp/config.js
@@ -1,5 +1,5 @@
 (function() {
-	var version = "4.7.13";
+	var version = "4.8.0";
 	var sNamespace = "shortpoint-widget-beta";
 	
 	var versionBuster = version.replace(/\./g, "00");
@@ -50,7 +50,7 @@
 	
 	var root = prefix + "/~" + versionBuster + "~/" + webapp;
 	jQuery.sap.registerModulePath("shortpoint-root", root);
-	jQuery.sap.registerModulePath("shortpoint-libs", "https://d2zzvnmppezkx.cloudfront.net/sap/4.7.12" + "/libs");
+	jQuery.sap.registerModulePath("shortpoint-libs", "https://d2zzvnmppezkx.cloudfront.net/sap/4.8.0" + "/libs");
 	
 	sap.ui.define(["./connections"], function (connections) {
 		"use strict";
