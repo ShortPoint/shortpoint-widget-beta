1b5b85c2752934e59707fb4983a89a3d4287c55c
Emmanuel Segun-Lean <34924520+LeanKhan@users.noreply.github.com>
2023-01-18 08:52:15 +0100

    Edge to Master v4.8.1 (#5)
    * increase version to 4.7.11
    
    * Revert "revert styles from CDN"
    
    This reverts commit 6f364e038fb4ec5b018c3a6f1cc28753a4552b22.
    
    * Hide preloader from shrotpoint styles
    
    * Increase version to 4.7.12
    
    * update style version
    
    * load styles after sap is fully loaded
    
    * change sap version
    
    * upgrade to version 4.7.13
    
    * SPX-5911 Fix Social Elements not showing in SAP
    
    * increase version to 4.8.0
    
    * remove console.log
    
    * increase version number to 4.8.1
    
    Co-authored-by: albertnahas <albert@shortpoint.com>
    Co-authored-by: Cavdy <cavdyofficials@gmail.com>

--- webapp/config.js
+++ webapp/config.js
@@ -1,5 +1,5 @@
 (function() {
-	var version = "4.7.11";
+	var version = "4.8.1";
 	var sNamespace = "shortpoint-widget-beta";
 	
 	var versionBuster = version.replace(/\./g, "00");
@@ -50,7 +50,7 @@
 	
 	var root = prefix + "/~" + versionBuster + "~/" + webapp;
 	jQuery.sap.registerModulePath("shortpoint-root", root);
-	jQuery.sap.registerModulePath("shortpoint-libs", "https://d2zzvnmppezkx.cloudfront.net/sap/4.7.11" + "/libs");
+	jQuery.sap.registerModulePath("shortpoint-libs", "https://d2zzvnmppezkx.cloudfront.net/sap/4.8.1" + "/libs");
 	
 	sap.ui.define(["./connections"], function (connections) {
 		"use strict";
